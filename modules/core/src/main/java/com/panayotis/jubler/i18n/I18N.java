8147348d6bf90ef033953e9efa0e04328a65521d
Hiram Kamau <hiram.kamau@outlook.com>
2022-03-01 20:19:17 +0300

    Update I18N.java

--- modules/core/src/main/java/com/panayotis/jubler/i18n/I18N.java
+++ modules/core/src/main/java/com/panayotis/jubler/i18n/I18N.java
@@ -28,7 +28,6 @@ import com.panayotis.jubler.os.DEBUG;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Locale;
